2c398d5c6238c5b2a89a49795bb04f3e6fa4f1a6
Rafael Costa <rafaelcosta8020@hotmail.com>
2020-01-02 21:55:45 -0300

    create delete option and optimize login function

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
 from db import *
 
 
-def verify(user, pw):
-    pass
-
-
 def main():
     while True:
-        print('-'*20)
-        print('  LOGIN SYSTEM  ')
-        print('-'*20)
-        print('Login - ("l")')
-        print('Create - ("c")')
-        print('Delete - ("d")')
-        print('Update - ("u")')
-        print('Show Users - ("s")')
-        print('Exit - ("e")')
-        answer = input('-> ').lower()
-        print()
+        answer = show_options()
 
-
-        if answer.startswith('l'):
-            print('\nLogin')
-            user = input('Username: ')
-            account = Account(user, None)
-            user_check = account.user_check()
-
-            if user_check:
-                password = input('Password: ')
-                account = Account(user, password)
-                pass_check = account.pw_check()
-                if pass_check:
-                    account.login()
-                else:
-                    print('Wrong Password\n')
-            else:
-                print('This username don\'t exist\n')
-
-
-        elif answer.startswith('c'):
+        if answer.startswith('c'):
             print('\nCreate')
             user = input('username: ')
             account = Account(user, None)
@@ -57,31 +24,76 @@ def main():
             else:
                 print('The user already exist\n')
 
+        elif answer.startswith('l'):
+            user = input('Username: ')
+            password = input('Password: ')
+            login(user, password)
 
         elif answer.startswith('d'):
-            pass
+            user = input('Username: ')
+            password = input('Password: ')
 
+            check = login(user, password)
+            if check:
+                print(f'Delete user: {user}?')
+                answer = input('Are you sure? (y/n): ')
+                if answer.startswith('y'):
+                    account = Account(user, None)
+                    account.delete()
+            print()
 
         elif answer.startswith('u'):
             pass
-
+            """
+            check = login()
+            print('What do you want to update?')
+            print('Password, Name or Message')
+            answer = ('-> ').lower()
+            """
 
         elif answer.startswith('s'):
             pass
 
-
         elif answer.startswith('e'):
             break
 
-
         else:
             print('Invalid Input')
-
             answer = input('Close the system? (y/n): ').lower()
             print()
-
             if answer.startswith('y'):
                 break
+
+
+def login(user, password):
+    account = Account(user, None)
+    user_check = account.user_check()
+
+    if user_check:
+        account = Account(user, password)
+        pass_check = account.pw_check()
+        if pass_check:
+            account.login()
+            return True
+        else:
+            print('Wrong Password\n')
+    else:
+        print('This username don\'t exist\n')
+
+
+def show_options():
+    print('-'*20)
+    print('  LOGIN SYSTEM  ')
+    print('-'*20)
+    print('Create - ("c")')
+    print('Login - ("l")')
+    print('Delete User - ("d")')
+    print('Update - ("u")')
+    print('Show Users - ("s")')
+    print('Exit - ("e")')
+    answer = input('-> ').lower()
+    print()
+    return answer
 
 
 if __name__ == '__main__':
